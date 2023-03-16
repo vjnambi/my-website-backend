5011221201e4b8a0be901f4044fbf0ae7872a9bc
vjnambi <vjnambi@gmail.com>
2023-03-16 14:14:53 -0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,21 +3,21 @@ require('dotenv').config();
 module.exports = {
     development: {
         username: "vjnambi",
-        password: process.env.MySQL_Password,
+        password: process.env.MYSQL_PASSWORD,
         database: "mywebsitedb",
         host: "vjnambidb.mysql.database.azure.com",
         dialect: "mysql"
     },
     test: {
         username: "vjnambi",
-        password: process.env.MySQL_Password,
+        password: process.env.MYSQL_PASSWORD,
         database: "mywebsitedb",
         host: "vjnambidb.mysql.database.azure.com",
         dialect: "mysql"
     },
     production: {
         username: "vjnambi",
-        password: process.env.MySQL_Password,
+        password: process.env.MYSQL_PASSWORD,
         database: "mywebsitedb",
         host: "vjnambidb.mysql.database.azure.com",
         dialect: "mysql"

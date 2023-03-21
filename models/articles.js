9c9074ed7f0164572f7b2df926f84b2dfcad43f0
vjnambi <vjnambi@gmail.com>
2023-03-20 22:05:51 -0700

    Changed articleText data type from STRING to TEXT

--- models/articles.js
+++ models/articles.js
@@ -3,7 +3,7 @@ module.exports = (sequelize, DataTypes) => {
     const articles = sequelize.define("articles",
     {
         title: {type: DataTypes.STRING, allowNull: false},
-        text: {type: DataTypes.STRING, allowNull: false}
+        text: {type: DataTypes.TEXT, allowNull: false}
     })
 
     return articles

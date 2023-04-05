d91c7c2f9629e256abc7863700c385b5aba23115
vjnambi <vjnambi@gmail.com>
2023-04-05 16:18:59 -0500

    attempting to use Azure app setting for password

--- config/config.js
+++ config/config.js
@@ -1,25 +1,25 @@
 require('dotenv').config();
 
 module.exports = {
-    development: {
-        username: "vjnambi",
-        password: env.MYSQL_PASSWORD,
-        database: "mywebsitedb",
-        host: "vjnambidb.mysql.database.azure.com",
-        dialect: "mysql"
+    "development": {
+        "username": "vjnambi",
+        "password": process.env.MYSQL_PASSWORD,
+        "database": "mywebsitedb",
+        "host": "vjnambidb.mysql.database.azure.com",
+        "dialect": "mysql"
     },
-    test: {
-        username: "vjnambi",
-        password: process.env.MYSQL_PASSWORD,
-        database: "mywebsitedb",
-        host: "vjnambidb.mysql.database.azure.com",
-        dialect: "mysql"
+    "test": {
+        "username": "vjnambi",
+        "password": process.env.MYSQL_PASSWORD,
+        "database": "mywebsitedb",
+        "host": "vjnambidb.mysql.database.azure.com",
+        "dialect": "mysql"
     },
-    production: {
-        username: "vjnambi",
-        password: process.env.MYSQL_PASSWORD,
-        database: "mywebsitedb",
-        host: "vjnambidb.mysql.database.azure.com",
-        dialect: "mysql"
+    "production": {
+        "username": "vjnambi",
+        "password": process.env.MYSQL_PASSWORD,
+        "database": "mywebsitedb",
+        "host": "vjnambidb.mysql.database.azure.com",
+        "dialect": "mysql"
     }
 };
